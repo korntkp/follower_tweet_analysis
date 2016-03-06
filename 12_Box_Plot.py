5630a97b67e9204b2dcf9933a78a500a938cad4d
shredktp <kornth_ebasicxp@hotmail.com>
2016-03-06 16:09:08 +0700

    Edit Dataframe

--- 12_Box_Plot.py
+++ 12_Box_Plot.py
@@ -26,12 +26,12 @@ def extract_diff_ret_or_fol(source_path_param, choose_str):
 
 
 def write_date_date_csv(output_path, list_diff_ret_param, list_diff_fol_param):
-    # fo = open(output_path, "w")
+    fo = open(output_path, "w")
     for i in range(0, len(list_diff_ret_param)):
-        result = str(list_diff_ret_param[i]) + " " + str(list_diff_fol_param[i]) + "\n"
+        result = str(list_diff_ret_param[i]) + "," + str(list_diff_fol_param[i]) + "\n"
         # print(result)  # TEST
-        # fo.write(result)
-    # fo.close()
+        fo.write(result)
+    fo.close()
     return
 
 
@@ -40,20 +40,20 @@ def pandas_df(data_path, topic_name, fold_num):
 
     # print(kde_follower)
     decomp_freq = int(18*7)
-    kde_follower_interpolated_bfill = kde_follower.DeltaFollower.interpolate().bfill()
+    # kde_follower_interpolated_bfill = kde_follower.DeltaFollower.interpolate().bfill()
 
     # SELECT GRAPH PLOT
     # 1 not Interpolate Plot
     # ori_plot = kde_follower.DeltaFollower.plot()
     # ori_plot.set_title("Topic: " + str(topic_name) + " Fold: " + str(fold_num))
     # 2 Interpolate Plot
-    ori_interpolated_plot = kde_follower.DeltaFollower.interpolate().plot()
-    ori_interpolated_plot.set_title("Topic: " + str(topic_name) + " Fold: " + str(fold_num))
+    # ori_interpolated_plot = kde_follower.DeltaFollower.interpolate().plot()
+    # ori_interpolated_plot.set_title("Topic: " + str(topic_name) + " Fold: " + str(fold_num))
     # 3 Decomposition Plot
     # res_plot = res_only_bfill.plot()
 
     # plt.show()
-    return kde_follower_interpolated_bfill
+    return kde_follower
 
 
 # SET PARAMETER
@@ -65,20 +65,20 @@ folds = ["1", "2", "3", "4", "5"]
 for each_choice in follower_choices:
     for each_topic in topics:
         for each_fold in folds:
+            print(each_topic, each_fold)
+
             source_path = "E:/tweet_process/result_follower-ret/06_diff_ret_fol_result/" + each_topic + "/fold_" + each_fold + "/all_tweet.csv"
             output_follower_csv = "E:/tweet_process/result_follower-ret/09_follower_retweet_csv_for_pandas/" + each_topic + "/fold_" + each_fold + "/all_tweet.csv"
 
-            list_diff_ret = extract_diff_ret_or_fol(source_path, 'retweet')
-            list_diff_fol = extract_diff_ret_or_fol(source_path, each_choice)
-
+            """
+            Read & Write Data
+            """
+            # list_diff_ret = extract_diff_ret_or_fol(source_path, 'retweet')
+            # list_diff_fol = extract_diff_ret_or_fol(source_path, each_choice)
+            # write_date_date_csv(output_follower_csv, list_diff_ret, list_diff_fol)
             # print(list_diff_fol)
-            print("===================================")
             # print(list_diff_ret)
-
-            """
-            Write Data
-            """
-            write_date_date_csv(output_follower_csv, list_diff_ret, list_diff_fol)
+            # print("================= End Read & Write Data ==================")
 
             df_follower = pandas_df(output_follower_csv, each_topic, each_fold)
-            # print(df_follower)
+            print(df_follower)
