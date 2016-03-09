40a009f4576baa90b7d2507e52643a9537e47bb7
shredktp <kornth_ebasicxp@hotmail.com>
2016-03-09 20:49:39 +0700

    Test KDE plot

--- 08_Kernel_Density_Estimation.py
+++ 08_Kernel_Density_Estimation.py
@@ -10,6 +10,9 @@ import pandas as pd
 import scipy.stats as stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+from sklearn.neighbors.kde import KernelDensity
 
 
 def process_data(source_path_param, output, choice):
@@ -266,7 +269,7 @@ for each_choice in y_axis_choices:
                 df_kde_value = pandas_plot(output_follower_csv, each_topic, each_fold)
             elif each_choice == 'retweet':
                 df_kde_value = pandas_plot(output_retweet_csv, each_topic, each_fold)
-            # print(df_kde_value)
+            print(df_kde_value)
 
             # """
             # Print TOP KDE delta_follower
@@ -278,3 +281,39 @@ for each_choice in y_axis_choices:
             # sorted_x.reverse()
             # print(len(sorted_x))
             # print(sorted_x[:10])
+
+            """
+            KDE Plot
+            """
+            # x = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+            # kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(x)
+            # asdf = kde.score_samples(x)
+            # print(asdf)
+            N = 100
+            np.random.seed(1)
+            X = np.concatenate((np.random.normal(0, 1, 0.3 * N),
+                                np.random.normal(5, 1, 0.7 * N)))[:, np.newaxis]
+
+            X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
+
+            true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0])
+                         + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
+
+            fig, ax = plt.subplots()
+            ax.fill(X_plot[:, 0], true_dens, fc='black', alpha=0.2,
+                    label='input distribution')
+
+            for kernel in ['gaussian', 'tophat', 'epanechnikov']:
+                kde = KernelDensity(kernel=kernel, bandwidth=0.5).fit(X)
+                log_dens = kde.score_samples(X_plot)
+                ax.plot(X_plot[:, 0], np.exp(log_dens), '-',
+                        label="kernel = '{0}'".format(kernel))
+
+            ax.text(6, 0.38, "N={0} points".format(N))
+
+            ax.legend(loc='upper left')
+            ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
+
+            ax.set_xlim(-4, 9)
+            ax.set_ylim(-0.02, 0.4)
+            plt.show()
