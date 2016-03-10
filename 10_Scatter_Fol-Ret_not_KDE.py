b0b9245ce5cca0ce883bf8b1a5515121860c4cb3
shredktp <kornth_ebasicxp@hotmail.com>
2016-03-10 18:37:09 +0700

    Remove Item more than Y

--- 10_Scatter_Fol-Ret_not_KDE.py
+++ 10_Scatter_Fol-Ret_not_KDE.py
@@ -94,15 +94,18 @@ def plot_diff_ret_and_diff_fol(list_ret, list_fol, choose_str, topic_name, fold_
 # SET PARAMETER
 # follower_choices = ['follower w/t mc', 'follower w/o mc']
 follower_choices = ['follower w/o mc']
-topics = ["apple", "aroii", "hormonestheseries", "thefacethailand"]
-folds = ["1", "2", "3", "4", "5"]
-# folds = ["1"]
+# topics = ["apple", "aroii", "hormonestheseries", "thefacethailand"]
+topics = ["apple"]
+# folds = ["1", "2", "3", "4", "5"]
+folds = ["1"]
+
 is_log_delta_retweet = True
 is_log_delta_follower = False
 logarithm_base_num = 2
+
 max_ret_plot = 15
-min_ret_plot = -5
-max_fol_plot = 15
+min_ret_plot = 0
+max_fol_plot = 200
 min_fol_plot = -5
 is_limit_axis = True
 
@@ -115,17 +118,30 @@ for each_choice in follower_choices:
             list_diff_fol = extract_diff_ret_or_fol(source_path, each_choice, is_log_delta_retweet, is_log_delta_follower, logarithm_base_num)
 
             print("============ Topic: " + each_topic + ", Fold: " + each_fold + ", " + each_choice + " =============")
-            print("Log Delta Retweet:  " + str(is_log_delta_retweet))
-            print("Log Delta Follower: " + str(is_log_delta_follower))
-            if is_log_delta_follower or is_log_delta_retweet:
-                print("Logarithm Base Number: " + str(logarithm_base_num))
-            print("Max of Delta retweet  -> " + str(max(list_diff_ret)))
-            print("Max of Delta follower -> " + str(max(list_diff_fol)))
-            print("Min of Delta retweet  -> " + str(min(list_diff_ret)))
-            print("Min of Delta follower -> " + str(min(list_diff_fol)))
+            # print("Log Delta Retweet:  " + str(is_log_delta_retweet))
+            # print("Log Delta Follower: " + str(is_log_delta_follower))
+            # if is_log_delta_follower or is_log_delta_retweet:
+            #     print("Logarithm Base Number: " + str(logarithm_base_num))
+            # print("Max of Delta retweet  -> " + str(max(list_diff_ret)))
+            # print("Max of Delta follower -> " + str(max(list_diff_fol)))
+            # print("Min of Delta retweet  -> " + str(min(list_diff_ret)))
+            # print("Min of Delta follower -> " + str(min(list_diff_fol)))
+
+            want_to_pop = []
+            for loop in range(0, len(list_diff_fol)):
+                if list_diff_fol[loop] > 10:
+                # if loop < 100:
+                #     print("Pop at: " + str(loop) + ", " + str(list_diff_fol[loop]))
+                    want_to_pop.append(loop)
+
+            count_outlier = 0
+            for pop_at in want_to_pop:
+                list_diff_fol.pop(pop_at - count_outlier)
+                list_diff_ret.pop(pop_at - count_outlier)
+                count_outlier += 1
 
             plot_diff_ret_and_diff_fol(list_diff_ret, list_diff_fol, each_choice, each_topic, each_fold, is_log_delta_retweet, is_log_delta_follower, is_limit_axis, max_fol_plot, max_ret_plot, min_fol_plot, min_ret_plot)
-            # print(scs.pearsonr(list_diff_ret, list_diff_fol))
-            # print(scs.spearmanr(list_diff_ret, list_diff_fol))
+            print(scs.pearsonr(list_diff_ret, list_diff_fol))
+            print(scs.spearmanr(list_diff_ret, list_diff_fol))
 
             # print(len(list_diff_fol), len(list_diff_ret))
