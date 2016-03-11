473cbc8c7312c53b589f02aa6999ccf5959a0ba5
shredktp <kornth_ebasicxp@hotmail.com>
2016-03-11 13:03:04 +0700

    sklearn KDE Plot

--- 08_Kernel_Density_Estimation.py
+++ 08_Kernel_Density_Estimation.py
@@ -266,9 +266,9 @@ for each_choice in y_axis_choices:
             Plot Y-Time Graph (Pandas)
             """
             if each_choice == 'follower_wo_mc':
-                df_kde_value = pandas_plot(output_follower_csv, each_topic, each_fold)
+                df_kde_value = pandas_plot(output_follower_csv, each_topic, each_fold)  # Date Time,Follower_count
             elif each_choice == 'retweet':
-                df_kde_value = pandas_plot(output_retweet_csv, each_topic, each_fold)
+                df_kde_value = pandas_plot(output_retweet_csv, each_topic, each_fold)  # Date Time,Retweet_count
             # print(df_kde_value)
 
             # """
@@ -289,38 +289,36 @@ for each_choice in y_axis_choices:
             # kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(x)
             # asdf = kde.score_samples(x)
             # print(asdf)
-            
+
             """
             KDE Plot 1 (sklearn.neighbors.kde)
             """
-            # N = 100
-            # np.random.seed(1)
+            N = df_kde_value.values.size
             # X = np.concatenate((np.random.normal(0, 1, 0.3 * N),
             #                     np.random.normal(5, 1, 0.7 * N)))[:, np.newaxis]
-            #
-            # X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
-            #
-            # true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0])
-            #              + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
-            #
-            # fig, ax = plt.subplots()
-            # ax.fill(X_plot[:, 0], true_dens, fc='black', alpha=0.2,
-            #         label='input distribution')
-            #
-            # for kernel in ['gaussian', 'tophat', 'epanechnikov']:
-            #     kde = KernelDensity(kernel=kernel, bandwidth=0.5).fit(X)
-            #     log_dens = kde.score_samples(X_plot)
-            #     ax.plot(X_plot[:, 0], np.exp(log_dens), '-',
-            #             label="kernel = '{0}'".format(kernel))
-            #
-            # ax.text(6, 0.38, "N={0} points".format(N))
-            #
-            # ax.legend(loc='upper left')
-            # ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
-            #
-            # ax.set_xlim(-4, 9)
-            # ax.set_ylim(-0.02, 0.4)
-            # plt.show()
+
+            X = df_kde_value.values[:, np.newaxis]
+            print(X)
+
+            X_plot = np.linspace(min(df_kde_value.values), max(df_kde_value.values), num=500)[:, np.newaxis]
+
+            true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0]) + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
+
+            fig, ax = plt.subplots()
+            ax.fill(X_plot, true_dens, fc='black', alpha=0.2,
+                    label='input distribution')
+
+            for kernel in ['gaussian', 'tophat', 'epanechnikov']:
+                kde = KernelDensity(kernel=kernel, bandwidth=0.05).fit(X)
+                log_dens = kde.score_samples(X_plot)
+                ax.plot(X_plot[:, 0], np.exp(log_dens), '-', label="kernel = '{0}'".format(kernel))
+
+            ax.text(6, 0.38, "N={0} points".format(N))
+            ax.legend(loc='upper right')
+            ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
+            ax.set_xlim(min(df_kde_value.values), max(df_kde_value.values))
+            ax.set_ylim(-0.02, 1)
+            plt.show()
 
             """
             KDE Plot 2 (scipy.stats) OK
@@ -344,6 +342,6 @@ for each_choice in y_axis_choices:
             # ax.plot(xs, y3, label='Const (1/3 * Silverman)')
             # ax.legend()
             # plt.show()
-            # # print(df_kde_value)
-            # # print(df_kde_value.shape)
-            # # print(df_kde_value.values.size)
+            # print(min(df_kde_value.values))
+            # print(df_kde_value.shape)
+            # print(df_kde_value.values.size)
