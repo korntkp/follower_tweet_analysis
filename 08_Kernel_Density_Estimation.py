79d838eb9d2cd5d3350a1c56d559d74763ef5d17
shredktp <kornth_ebasicxp@hotmail.com>
2016-03-12 19:23:50 +0700

    Save

--- 08_Kernel_Density_Estimation.py
+++ 08_Kernel_Density_Estimation.py
@@ -203,13 +203,39 @@ def plot_diff_ret_and_diff_fol(list_ret, list_fol, choose_str, topic_name, fold_
     plt.show()
 
 
+def stats_gaussian_kde_plot(dataframe):
+    kde = stats.gaussian_kde(df_kde_value.values)
+    xs = np.linspace(-10, 10, num=50)
+    y1 = kde(xs)
+    print(kde.factor)
+    kde.set_bandwidth(bw_method='silverman')
+    y2 = kde(xs)
+    print(kde.factor)
+    kde.set_bandwidth(bw_method=kde.factor / 3)  # 100 - 1000
+    # kde.set_bandwidth(bw_method=)
+    y3 = kde(xs)
+    print(kde.factor)
+
+    fig, ax = plt.subplots()
+    ax.plot(df_kde_value.values, np.ones(df_kde_value.values.shape) / (4. * df_kde_value.values.size), 'bo', label='Data points (rescaled)')
+    # ax.plot(xs, y1, label='Scott (default)')
+    # ax.plot(xs, y2, label='Silverman')
+    ax.plot(xs, y3, label='Const (1/3 * Silverman)')
+    ax.legend()
+    plt.show()
+    # print(min(df_kde_value.values))
+    # print(df_kde_value.shape)
+    # print(df_kde_value.values.size)
+
+
+
 # y_axis_choices = ['retweet', 'follower_wt_mc', 'follower_wo_mc']
 y_axis_choices = ['follower_wo_mc']
 # y_axis_choices = ['retweet']
 topics = ["apple", "aroii", "hormonestheseries", "thefacethailand"]
-# topics = ["aroii"]
-# folds = ["1", "2", "3", "4", "5"]
-folds = ["1"]
+# topics = ["thefacethailand"]
+folds = ["1", "2", "3", "4", "5"]
+# folds = ["5"]
 
 unix_time_start = [1447023600, 1447714800]  # 2015-11-09 06:00:00   2015-11-17 06:00:00
 last_hour_app_aroii = 1651
@@ -219,7 +245,7 @@ for each_choice in y_axis_choices:
     for each_topic in topics:
         for each_fold in folds:
 
-            print(each_topic, each_fold)
+            # print(each_topic, each_fold)
             data = []  # Array for collect original data
             data_estimate = []  # Array for collect KDE processed data
 
@@ -283,65 +309,50 @@ for each_choice in y_axis_choices:
             # print(sorted_x[:10])
 
             """
-            KDE Plot
-            """
-            # x = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-            # kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(x)
-            # asdf = kde.score_samples(x)
-            # print(asdf)
-
-            """
             KDE Plot 1 (sklearn.neighbors.kde)
             """
             N = df_kde_value.values.size
-            # X = np.concatenate((np.random.normal(0, 1, 0.3 * N),
-            #                     np.random.normal(5, 1, 0.7 * N)))[:, np.newaxis]
-
             X = df_kde_value.values[:, np.newaxis]
-            print(X)
 
             X_plot = np.linspace(min(df_kde_value.values), max(df_kde_value.values), num=500)[:, np.newaxis]
+            # X_plot = np.linspace(min(df_kde_value.values), 10, num=500)[:, np.newaxis]
+            # print(min(df_kde_value.values))
+            print(max(df_kde_value.values))
+            # print(df_kde_value)
 
             true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0]) + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
 
             fig, ax = plt.subplots()
-            ax.fill(X_plot, true_dens, fc='black', alpha=0.2,
-                    label='input distribution')
+            ax.fill(X_plot, true_dens, fc='black', alpha=0.2, label='input distribution')
 
-            for kernel in ['gaussian', 'tophat', 'epanechnikov']:
-                kde = KernelDensity(kernel=kernel, bandwidth=0.05).fit(X)
-                log_dens = kde.score_samples(X_plot)
-                ax.plot(X_plot[:, 0], np.exp(log_dens), '-', label="kernel = '{0}'".format(kernel))
+            # for kernel in ['gaussian', 'tophat', 'epanechnikov']:
+            #     kde = KernelDensity(kernel='gaussian', bandwidth=0.005).fit(X)
+            #     log_dens = kde.score_samples(X_plot)
+            #     ax.plot(X_plot[:, 0], np.exp(log_dens), '-', label="kernel = '{0}'".format(kernel))
+
+            # kde = KernelDensity(kernel='gaussian', bandwidth=0.005).fit(X)
+            kde = KernelDensity(kernel='gaussian', bandwidth=0.05).fit(X)
+            # print(kde.score(X))
+            # print(kde.score_samples(X))
+            # print(kde.score_samples(X_plot))
+
+            # print(len(kde.score_samples(X)))
+            # print(len(kde.score_samples(X_plot)))
+
+            log_dens = kde.score_samples(X_plot)
+            ax.plot(X_plot[:, 0], np.exp(log_dens), '-', label="kernel = '{0}'".format('gaussian'))
 
             ax.text(6, 0.38, "N={0} points".format(N))
             ax.legend(loc='upper right')
             ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
+
             ax.set_xlim(min(df_kde_value.values), max(df_kde_value.values))
             ax.set_ylim(-0.02, 1)
-            plt.show()
+            plt.title('Density - ' + each_choice + ' (' + each_topic + ', ' + each_fold + ')')
+            # plt.show()
 
             """
             KDE Plot 2 (scipy.stats) OK
             """
-            # kde = stats.gaussian_kde(df_kde_value.values)
-            # xs = np.linspace(-10, 10, num=50)
-            # y1 = kde(xs)
-            # print(kde.factor)
-            # kde.set_bandwidth(bw_method='silverman')
-            # y2 = kde(xs)
-            # print(kde.factor)
-            # kde.set_bandwidth(bw_method=kde.factor / 3)  # 100 - 1000
-            # # kde.set_bandwidth(bw_method=)
-            # y3 = kde(xs)
-            # print(kde.factor)
-            #
-            # fig, ax = plt.subplots()
-            # ax.plot(df_kde_value.values, np.ones(df_kde_value.values.shape) / (4. * df_kde_value.values.size), 'bo', label='Data points (rescaled)')
-            # # ax.plot(xs, y1, label='Scott (default)')
-            # # ax.plot(xs, y2, label='Silverman')
-            # ax.plot(xs, y3, label='Const (1/3 * Silverman)')
-            # ax.legend()
-            # plt.show()
-            # print(min(df_kde_value.values))
-            # print(df_kde_value.shape)
-            # print(df_kde_value.values.size)
+            # stats_gaussian_kde_plot(df_kde_value)
+
