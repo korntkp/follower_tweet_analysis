74324148e425b9a5b72aeb8ffca1d47a7aa11cb0
shredktp <kornth_ebasicxp@hotmail.com>
2016-04-02 19:39:06 +0700

    Add shade area label

--- 14_Intersect_Trend.py
+++ 14_Intersect_Trend.py
@@ -62,13 +62,26 @@ def two_plot_before(before_scale_retweet, before_scale_follower, topic_name, fol
     return
 
 
-def two_plot_after(after_scale_retweet, after_scale_follower, topic_name, fold_num):
+def two_plot_after(after_scale_retweet, after_scale_follower, topic_name, fold_num, shade_option):
     # print("dfgjhjtrter")
     fig, ax = plt.subplots()
 
     ax.plot(range(0, len(after_scale_retweet)), after_scale_retweet, '-', linewidth=0.8, color='green', label='Trend Delta Retweet')
     ax.plot(range(0, len(after_scale_follower)), after_scale_follower, '-', linewidth=3.5, color='black', label='Trend Delta Follower')
-
+    """
+    shade_option = ['none', 'Union', 'Non-Intersect', 'Intersect']
+    """
+    if shade_option == 'Union':
+        print("Union")
+        ax.fill_between(range(0, 1), 0, facecolor='black', alpha=0.5, label='Union Area')    # Manual Fill Color
+    elif shade_option == 'Non-Intersect':
+        print("Non-Intersect")
+        ax.fill_between(range(0, 1), 0, facecolor='black', alpha=0.5, label='Non Intersect Area')    # Manual Fill Color
+    elif shade_option == 'Intersect':
+        print("Intersect")
+        ax.fill_between(range(0, 1), 0, facecolor='black', alpha=0.5, label='Intersect Area')    # Manual Fill Color
+    else:
+        print("none")
 
     ax.set_xlabel("Time (Hour)")
     ax.set_ylabel('Trend Value After Scaling')
@@ -143,10 +156,13 @@ def plot_union(union_list, after_scale_retweet, after_scale_follower, topic_name
 
 # y_axis_choices = ['follower_wo_mc']
 y_axis_choices = ['retweet']
-topics = ["apple", "aroii", "hormonestheseries", "thefacethailand"]
-# topics = ["hormonestheseries"]
+# topics = ["apple", "aroii", "hormonestheseries", "thefacethailand"]
+topics = ["hormonestheseries"]
 # folds = ["1", "2", "3", "4", "5"]
-folds = ["1"]
+folds = ["1", "2", "3"]
+
+shade_area_num_param = 0
+shade_area = ['none', 'Union', 'Non-Intersect', 'Intersect']
 
 for each_choice in y_axis_choices:
     for each_topic in topics:
@@ -275,9 +291,9 @@ for each_choice in y_axis_choices:
             """
             # print("Plot Before")
             # print("Plot After")
-            one_plot_before(only_value_retweet, only_value_follower, each_topic, each_fold, each_choice)
+            # one_plot_before(only_value_retweet, only_value_follower, each_topic, each_fold, each_choice)
             # two_plot_before(only_value_retweet, only_value_follower, each_topic, each_fold)
-            # two_plot_after(new_scale_retweet, new_scale_follower, each_topic, each_fold)        # THIS
+            two_plot_after(new_scale_retweet, new_scale_follower, each_topic, each_fold, shade_area[shade_area_num_param])        # THIS
             # plot_union(for_plot_union_area, new_scale_retweet, new_scale_follower, each_topic, each_fold, for_plot_union_area_plus, for_plot_union_area_minus)
             # four_plot(only_value_retweet, new_scale_retweet, only_value_follower, new_scale_follower, 'follower_wo_mc', each_topic, each_fold)
 
